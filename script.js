09cb7353aa21589924ec9a3029f0fd854c8770b1
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-08 19:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function firstChar(text) {
   // your code here
+	if(text==="") return "";
+	else{
 	let ans="";
-	if(text.length===0||(text.length===1 && text.charAt(0)===" ")) return ans;
-	for(let i=0;i<text.length;){
-		if(text.charAt(i)===" "){
-			i++;
+	for(let i=0;i<text.length;i++){
+		if(text.charAt(i)!=" "){
+			ans+=text.charAt(i);
+			break;
 		}
-		else{
-			return text.charAt(i);
-		}
+	}
+		return ans;
 	}
 }
 // Do not change the code below
